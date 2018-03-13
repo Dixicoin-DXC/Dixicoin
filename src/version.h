2b929244d448700423f737390536376244cdb3d3
Dixicoin-DXC <36902928+Dixicoin-DXC@users.noreply.github.com>
2018-03-13 09:45:09 -0300

    Protocol Version

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70710;
+static const int PROTOCOL_VERSION = 70711;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
